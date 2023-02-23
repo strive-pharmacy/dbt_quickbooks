84f9a350e7c1e9b933b2fd43d7316288a5c85c47
graydonclark <42157268+graydonclark@users.noreply.github.com>
2023-02-23 00:28:46 -0700

    Update quickbooks__general_ledger.sql

--- models/quickbooks__general_ledger.sql
+++ models/quickbooks__general_ledger.sql
@@ -28,7 +28,7 @@ accounts as (
 
 adjusted_gl as (
     
-    select
+    select distinct
         {{ dbt_utils.generate_surrogate_key(['gl_union.transaction_id', 'gl_union.index', 'gl_union.account_id', ' gl_union.transaction_type', 'gl_union.transaction_source']) }} as unique_id,
         gl_union.transaction_id,
         gl_union.source_relation,
